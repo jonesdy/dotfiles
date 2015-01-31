782887f00d316fcb3b01e841ebfacb1f87a73c62
Dylan Jones <jonesdyn@gmail.com>
2015-01-30 23:21:37 -0600

    Adding the symlinks back in because they do work, once

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ for filename in $dir* $dir.*; do
    dotfile=${filename#$dir}
    if [ $filename != $script -a $dotfile != "." -a $dotfile != ".." -a $dotfile != ".git" -a $dotfile != ".config" ]
       then
-#         ln -sf $filename ~/$dotfile
-         echo $filename
-         echo ~/$dotfile
+         ln -sf $filename ~/$dotfile
+#         echo $filename
+#         echo ~/$dotfile
    fi
 done
 
@@ -17,8 +17,8 @@ for filename in $dir* $dir.*; do
    dotfile=${filename#$dir}
    if [ $filename != $script -a $dotfile != "." -a $dotfile != ".." -a $dotfile != ".git" -a $dotfile != ".config" ]
       then
-#         ln -sf $filename ~/.config/$dotfile
-         echo $filename
-         echo ~/.config/$dotfile
+         ln -sf $filename ~/.config/$dotfile
+#         echo $filename
+ #        echo ~/.config/$dotfile
    fi
 done
