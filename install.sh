4a717724b66cb8e490818bf3a03663c206fca868
Dylan Jones <jonesdyn@gmail.com>
2015-01-31 00:23:20 -0600

    Adding submodule stuff to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 script=$(readlink -f ${BASH_SOURCE[0]})
 
+# Update the submodules first
+git submodule init
+git submodule update
+
 for filename in $dir* $dir.*; do
    dotfile=${filename#$dir}
    if [ $filename != $script -a $dotfile != "." -a $dotfile != ".." -a $dotfile != ".git" -a $dotfile != ".config" ]
